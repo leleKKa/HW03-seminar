05a3451d0886da6eba6875f114288da06bf09f56
Olga <oadotik@gmail.com>
2023-03-26 17:57:56 +0300

    первая задача

--- homework/HW5/Program.cs
+++ homework/HW5/Program.cs
@@ -1,12 +1,13 @@
 ﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
 Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2*/
-/*
+
 int[]CreateRandomArray(int size, int minValue, int maxValue)
 {
   int[] array = new int[size];
   for(int i = 0; i < size; i++)
   {
+    if (minValue >= 100 && maxValue < 1000)// нужно ли тут это условие
     array[i] = new Random().Next(minValue, maxValue + 1);
   }
   return array;
@@ -42,7 +43,7 @@ WriteArray(myArray);
 
 int result = Number(myArray);
 Console.WriteLine("Количество четных элементов в массиве " + result);
-*/
+
 
 /*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
 Найдите сумму элементов, стоящих на нечётных позициях.
